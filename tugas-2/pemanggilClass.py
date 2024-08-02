d5e95d0a13769c756bc34bd52a5c0d4ed9c15b46
MochArisRofii <mochrofii827@gmail.com>
2024-08-02 11:13:34 +0700

    gass

--- tugas-2/pemanggilClass.py
+++ tugas-2/pemanggilClass.py
@@ -3,8 +3,8 @@ from FilClass import HandleFile
 def Baca_Class():
     file = HandleFile()
      
+    file.baca_file('Output.txt')
+    
     file.tulis_file('Output.txt', 'a') # Bisa Ganti Mode Sesuka Hati "tulis_file("nama_file", "mode")"
-
-    # file.baca_file('Output.txt')
 
 Baca_Class()
